681ada342acf1dd76603535549c4169b8bb17640
wzeng <wzeng2002@gmail.com>
2024-07-26 16:14:34 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false, // Disable Node.js integration
+      contextIsolation: true, // Enable context isolation
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Hide the menu bar
+  //mainWindow.setMenuBarVisibility(false);
+
+  // Open the DevTools (optional)
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
