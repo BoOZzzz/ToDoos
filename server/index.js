d56fbac10d037f7f93490b3bf003b066f92d40a1
wzeng <wzeng2002@gmail.com>
2024-08-15 20:31:17 -0400

    changed db directory

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 // Initialize a new Sequelize instance with SQLite
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite' // Path to SQLite file
+  storage: './db/database.sqlite' // Path to SQLite file
 });
 
 // Export the sequelize instance
